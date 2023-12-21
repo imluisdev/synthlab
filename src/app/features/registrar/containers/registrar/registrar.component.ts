7caf32d0d63192142edf0ee28287844d75e6781f
Luis Godinez <a20310061@gmail.com>
2023-12-20 22:14:06 -0600

    navbar fixed and more changes to register user

--- src/app/features/registrar/containers/registrar/registrar.component.ts
+++ src/app/features/registrar/containers/registrar/registrar.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { IRegistrarUsuarioGroup } from '../../../../models/form.models';
 import { IRegistrarUsuario } from '../../../../models/usuario.models';
 import { UsuarioService } from '../../../../services/usuario.service';
-import { finalize } from 'rxjs';
+import { finalize, take, timer } from 'rxjs';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 
@@ -19,9 +19,21 @@ export class RegistrarComponent implements OnInit {
   public registrarUsuarioGroup: FormGroup<IRegistrarUsuarioGroup>;
   public loading: boolean;
   public hiddenPassword: boolean = true;
+  public hiddenConfirmationPassword: boolean = true;
+  public hasCapitalLetter: boolean = false;
+  public hasNumber: boolean = false;
+  public hasSymbol: boolean = false;
 
   ngOnInit() {
     this.armarFormularioRegistro();
+  }
+
+  public hola(){
+
+    this.hasCapitalLetter = /[A-Z]/.test(this.registrarUsuarioGroup.controls['contrasena'].value);
+    this.hasNumber = /\d/.test(this.registrarUsuarioGroup.controls['contrasena'].value);
+    this.hasSymbol = /[^a-zA-Z0-9]/.test(this.registrarUsuarioGroup.controls['contrasena'].value);
+
   }
 
   public armarFormularioRegistro(){
@@ -31,22 +43,34 @@ export class RegistrarComponent implements OnInit {
       apellido_materno: ['', Validators.required],
       correo_electronico: ['', [Validators.required, Validators.email]],
       contrasena: ['', [Validators.required, Validators.minLength(8)]],
+      confirmar_contrasena: ['', [Validators.required, Validators.minLength(8)]],
       fecha_nacimiento: ['', Validators.required],
     });
   }
 
   public registrarUsuario(){
     if(this.registrarUsuarioGroup.valid){
-      const req: IRegistrarUsuario = {
-        nombre: this.registrarUsuarioGroup.controls['nombre'].value,
-        apellido_paterno: this.registrarUsuarioGroup.controls['apellido_paterno'].value,
-        apellido_materno: this.registrarUsuarioGroup.controls['apellido_materno'].value,
-        correo_electronico: this.registrarUsuarioGroup.controls['correo_electronico'].value,
-        contrasena: this.registrarUsuarioGroup.controls['contrasena'].value,
-        fecha_nacimiento: this.registrarUsuarioGroup.controls['fecha_nacimiento'].value,
-      };
-      return;
-      this.peticionRegistrarUsuario(req);
+      const password = this.registrarUsuarioGroup.controls['contrasena'].value;
+      const confirmationPassword = this.registrarUsuarioGroup.controls['confirmar_contrasena'].value;
+      if(password == confirmationPassword){
+        const req: IRegistrarUsuario = {
+          nombre: this.registrarUsuarioGroup.controls['nombre'].value,
+          apellido_paterno: this.registrarUsuarioGroup.controls['apellido_paterno'].value,
+          apellido_materno: this.registrarUsuarioGroup.controls['apellido_materno'].value,
+          correo_electronico: this.registrarUsuarioGroup.controls['correo_electronico'].value,
+          contrasena: this.registrarUsuarioGroup.controls['contrasena'].value,
+          fecha_nacimiento: this.registrarUsuarioGroup.controls['fecha_nacimiento'].value,
+        };
+        this.peticionRegistrarUsuario(req);
+      } else {
+        Swal.fire({
+          title: "<h1 class='font-bold'>Error en el registro</h1>",
+          html: `<h1 class='font-semibold mb-5'>Las contraseñas proporcionadas no coinciden</h1>`,
+          icon: "error",
+          showConfirmButton: false,
+          timer: 2000
+        });
+      }
     } else {
       Swal.fire({
         title: "<h1 class='font-bold'>Error en el registro</h1>",
@@ -59,7 +83,27 @@ export class RegistrarComponent implements OnInit {
   }
 
   public changePasswordVisibility(){
-    this.hiddenPassword = !this.hiddenPassword;
+    if(this.hiddenPassword){
+      this.hiddenPassword = !this.hiddenPassword;
+      const timer$ = timer(1000);
+      timer$.pipe(
+        take(1)
+      ).subscribe(() => {
+        this.hiddenPassword = true;
+      });
+    }
+  }
+
+  public changeConfirmationPasswordVisibility(){
+    if(this.hiddenConfirmationPassword){
+      this.hiddenConfirmationPassword = !this.hiddenConfirmationPassword;
+      const timer$ = timer(1000);
+      timer$.pipe(
+        take(1)
+      ).subscribe(() => {
+        this.hiddenConfirmationPassword = true;
+      });
+    }
   }
 
   public peticionRegistrarUsuario(req: IRegistrarUsuario){
@@ -79,6 +123,14 @@ export class RegistrarComponent implements OnInit {
           showConfirmButton: false,
           timer: 2000,
           didClose: () => this.router.navigate(['/'])
+        });
+      } else {
+        Swal.fire({
+          title: "<h1 class='font-bold'>Error al realizar el registro</h1>",
+          html: `<h1 class='font-semibold mb-5'>${ resp.message }</h1>`,
+          icon: "error",
+          showConfirmButton: false,
+          timer: 2000
         });
       }
     });
