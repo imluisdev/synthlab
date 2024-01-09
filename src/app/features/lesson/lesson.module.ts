4d40b5c4723f5072da63d0e22cee102114547f7e
juanlomelifuentes <juankw44@gmail.com>
2024-01-08 18:34:24 -0600

    lesson front creation

--- src/app/features/lesson/lesson.module.ts
+++ src/app/features/lesson/lesson.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LessonRoutingModule } from './lesson-routing.module';
 import { LessonComponent } from './containers/lesson/lesson.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,11 @@ import { LessonComponent } from './containers/lesson/lesson.component';
   ],
   imports: [
     CommonModule,
-    LessonRoutingModule
-  ]
+    LessonRoutingModule,
+    SharedModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class LessonModule { }
+export class LessonModule {
+    
+}
