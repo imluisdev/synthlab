1082bddacf01e3b5cd289bed37dfb0f15f740547
juanlomelifuentes <juankw44@gmail.com>
2024-05-09 20:33:54 -0600

    midi functionality added to the synth

--- src/app/features/synth/containers/synth/synth.component.ts
+++ src/app/features/synth/containers/synth/synth.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class SynthComponent {
   osc_unison: number = 0;
   osc_wave: number = 0;
-  frequency_filter: number = 9500;
-  resonance: number = 5;
+  frequency_filter: number = 70;
+  resonance: number = 1;
   attack: number = 0.1;
   decay: number = 0.2;
   sustain: number = 0.5;
@@ -24,6 +24,7 @@ export class SynthComponent {
   svgElement: HTMLElement | null; 
   svgLFO: HTMLElement | null;
   waveform: OscillatorType = 'sine';
+  audioContext: AudioContext = new AudioContext();
 
   buttonStates: { [key: string]: boolean } = {};
 
@@ -38,6 +39,7 @@ export class SynthComponent {
     this.updateWave();
     this.svgLFO = document.getElementById('wavePathLFO');
     this.updateWaveLFO();
+    this.requestMIDIAccess();
   }
 
   handleMouseOver(element: string, description: string): void {
@@ -50,6 +52,378 @@ export class SynthComponent {
     this.isHovered = false;
     this.hoveredElement = '¿Que elementos componen un sintetizador?';
     this.hoveredElementDescription = 'Coloca el mouse sobre un elemento del sintetizador para saber más sobre el';
+  }
+
+  async requestMIDIAccess() {
+    try {
+      const midiAccess = await navigator.requestMIDIAccess();
+
+      midiAccess.inputs.forEach((input) => {
+        input.onmidimessage = this.onMIDIMessage.bind(this);
+      });
+    } catch (error) {
+      console.error('Error al acceder a los dispositivos MIDI:', error);
+    }
+  }
+
+  onMIDIMessage(event: any) {
+    const [command, note] = event.data;
+
+    if (command === 144) {
+      switch (note) {
+        case 48:
+            this.playNote('C2');
+            this.changeColor('C2', true);
+            break;
+        case 49:
+            this.playNote('Db2');
+            this.changeColor('Db2', true);
+            break;
+        case 50:
+            this.playNote('D2');
+            this.changeColor('D2', true);
+            break;
+        case 51:
+            this.playNote('Eb2');
+            this.changeColor('Eb2', true);
+            break;
+        case 52:
+            this.playNote('E2');
+            this.changeColor('E2', true);
+            break;
+        case 53:
+            this.playNote('F2');
+            this.changeColor('F2', true);
+            break;
+        case 54:
+            this.playNote('Gb2');
+            this.changeColor('Gb2', true);
+            break;
+        case 55:
+            this.playNote('G2');
+            this.changeColor('G2', true);
+            break;
+        case 56:
+            this.playNote('Ab2');
+            this.changeColor('Ab2', true);
+            break;
+        case 57:
+            this.playNote('A2');
+            this.changeColor('A2', true);
+            break;
+        case 58:
+            this.playNote('Bb2');
+            this.changeColor('Bb2', true);
+            break;
+        case 59:
+            this.playNote('B2');
+            this.changeColor('B2', true);
+            break;
+        case 60:
+            this.playNote('C3');
+            this.changeColor('C3', true);
+            break;
+        case 61:
+            this.playNote('Db3');
+            this.changeColor('Db3', true);
+            break;
+        case 62:
+            this.playNote('D3');
+            this.changeColor('D3', true);
+            break;
+        case 63:
+            this.playNote('Eb3');
+            this.changeColor('Eb3', true);
+            break;
+        case 64:
+            this.playNote('E3');
+            this.changeColor('E3', true);
+            break;
+        case 65:
+            this.playNote('F3');
+            this.changeColor('F3', true);
+            break;
+        case 66:
+            this.playNote('Gb3');
+            this.changeColor('Gb3', true);
+            break;
+        case 67:
+            this.playNote('G3');
+            this.changeColor('G3', true);
+            break;
+        case 68:
+            this.playNote('Ab3');
+            this.changeColor('Ab3', true);
+            break;
+        case 69:
+            this.playNote('A3');
+            this.changeColor('A3', true);
+            break;
+        case 70:
+            this.playNote('Bb3');
+            this.changeColor('Bb3', true);
+            break;
+        case 71:
+            this.playNote('B3');
+            this.changeColor('B3', true);
+            break;
+        case 72:
+            this.playNote('C4');
+            this.changeColor('C4', true);
+            break;
+        case 73:
+            this.playNote('Db4');
+            this.changeColor('Db4', true);
+            break;
+        case 74:
+            this.playNote('D4');
+            this.changeColor('D4', true);
+            break;
+        case 75:
+            this.playNote('Eb4');
+            this.changeColor('Eb4', true);
+            break;
+        case 76:
+            this.playNote('E4');
+            this.changeColor('E4', true);
+            break;
+        case 77:
+            this.playNote('F4');
+            this.changeColor('F4', true);
+            break;
+        case 78:
+            this.playNote('Gb4');
+            this.changeColor('Gb4', true);
+            break;
+        case 79:
+            this.playNote('G4');
+            this.changeColor('G4', true);
+            break;
+        case 80:
+            this.playNote('Ab4');
+            this.changeColor('Ab4', true);
+            break;
+        case 81:
+            this.playNote('A4');
+            this.changeColor('A4', true);
+            break;
+        case 82:
+            this.playNote('Bb4');
+            this.changeColor('Bb4', true);
+            break;
+        case 83:
+            this.playNote('B4');
+            this.changeColor('B4', true);
+            break;
+        case 84:
+            this.playNote('C5');
+            this.changeColor('C5', true);
+            break;
+        case 85:
+            this.playNote('Db5');
+            this.changeColor('Db5', true);
+            break;
+        case 86:
+            this.playNote('D5');
+            this.changeColor('D5', true);
+            break;
+        case 87:
+            this.playNote('Eb5');
+            this.changeColor('Eb5', true);
+            break;
+        case 88:
+            this.playNote('E5');
+            this.changeColor('E5', true);
+            break;
+        case 89:
+            this.playNote('F5');
+            this.changeColor('F5', true);
+            break;
+        case 90:
+            this.playNote('Gb5');
+            this.changeColor('Gb5', true);
+            break;
+        case 91:
+            this.playNote('G5');
+            this.changeColor('G5', true);
+            break;
+        case 92:
+            this.playNote('Ab5');
+            this.changeColor('Ab5', true);
+            break;
+        case 93:
+            this.playNote('A5');
+            this.changeColor('A5', true);
+            break;
+        case 94:
+            this.playNote('Bb5');
+            this.changeColor('Bb5', true);
+            break;
+        case 95:
+            this.playNote('B5');
+            this.changeColor('B5', true);
+            break;
+        case 96:
+            this.playNote('C6');
+            this.changeColor('C6', true);
+            break;
+        default:
+            break;
+      }
+    }
+    if (command === 128) {
+      switch (note) {
+        case 48:
+            this.changeColor('C2', false);
+            break;
+        case 49:
+            this.changeColor('Db2', false);
+            break;
+        case 50:
+            this.changeColor('D2', false);
+            break;
+        case 51:
+            this.changeColor('Eb2', false);
+            break;
+        case 52:
+            this.changeColor('E2', false);
+            break;
+        case 53:
+            this.changeColor('F2', false);
+            break;
+        case 54:
+            this.changeColor('Gb2', false);
+            break;
+        case 55:
+            this.changeColor('G2', false);
+            break;
+        case 56:
+            this.changeColor('Ab2', false);
+            break;
+        case 57:
+            this.changeColor('A2', false);
+            break;
+        case 58:
+            this.changeColor('Bb2', false);
+            break;
+        case 59:
+            this.changeColor('B2', false);
+            break;
+        case 60:
+            this.changeColor('C3', false);
+            break;
+        case 61:
+            this.changeColor('Db3', false);
+            break;
+        case 62:
+            this.changeColor('D3', false);
+            break;
+        case 63:
+            this.changeColor('Eb3', false);
+            break;
+        case 64:
+            this.changeColor('E3', false);
+            break;
+        case 65:
+            this.changeColor('F3', false);
+            break;
+        case 66:
+            this.changeColor('Gb3', false);
+            break;
+        case 67:
+            this.changeColor('G3', false);
+            break;
+        case 68:
+            this.changeColor('Ab3', false);
+            break;
+        case 69:
+            this.changeColor('A3', false);
+            break;
+        case 70:
+            this.changeColor('Bb3', false);
+            break;
+        case 71:
+            this.changeColor('B3', false);
+            break;
+        case 72:
+            this.changeColor('C4', false);
+            break;
+        case 73:
+            this.changeColor('Db4', false);
+            break;
+        case 74:
+            this.changeColor('D4', false);
+            break;
+        case 75:
+            this.changeColor('Eb4', false);
+            break;
+        case 76:
+            this.changeColor('E4', false);
+            break;
+        case 77:
+            this.changeColor('F4', false);
+            break;
+        case 78:
+            this.changeColor('Gb4', false);
+            break;
+        case 79:
+            this.changeColor('G4', false);
+            break;
+        case 80:
+            this.changeColor('Ab4', false);
+            break;
+        case 81:
+            this.changeColor('A4', false);
+            break;
+        case 82:
+            this.changeColor('Bb4', false);
+            break;
+        case 83:
+            this.changeColor('B4', false);
+            break;
+        case 84:
+            this.changeColor('C5', false);
+            break;
+        case 85:
+            this.changeColor('Db5', false);
+            break;
+        case 86:
+            this.changeColor('D5', false);
+            break;
+        case 87:
+            this.changeColor('Eb5', false);
+            break;
+        case 88:
+            this.changeColor('E5', false);
+            break;
+        case 89:
+            this.changeColor('F5', false);
+            break;
+        case 90:
+            this.changeColor('Gb5', false);
+            break;
+        case 91:
+            this.changeColor('G5', false);
+            break;
+        case 92:
+            this.changeColor('Ab5', false);
+            break;
+        case 93:
+            this.changeColor('A5', false);
+            break;
+        case 94:
+            this.changeColor('Bb5', false);
+            break;
+        case 95:
+            this.changeColor('B5', false);
+            break;
+        case 96:
+            this.changeColor('C6', false);
+            break;
+        default:
+            break;
+      }
+    }
   }
 
   initButtonStates() {
@@ -129,15 +503,14 @@ export class SynthComponent {
 
     const createOscillators = (osc_unison: number) => {
       const frequency = frequencies[note];
-      const audioContext = new AudioContext();
-      const osc = audioContext.createOscillator();
-      const gainNode = audioContext.createGain();
-      const filterNode = audioContext.createBiquadFilter();
-      const lfo = audioContext.createOscillator();
-      const lfoGain = audioContext.createGain();
+      const osc = this.audioContext.createOscillator();
+      const gainNode = this.audioContext.createGain();
+      const filterNode = this.audioContext.createBiquadFilter();
+      const lfo = this.audioContext.createOscillator();
+      const lfoGain = this.audioContext.createGain();
       osc.connect(filterNode);
       filterNode.connect(gainNode);
-      gainNode.connect(audioContext.destination); 
+      gainNode.connect(this.audioContext.destination); 
 
       lfo.connect(lfoGain);
       lfoGain.connect(osc.frequency);
@@ -157,14 +530,14 @@ export class SynthComponent {
       filterNode.frequency.value = this.frequency_filter;
       filterNode.Q.value = this.resonance;
 
-      const now = audioContext.currentTime;
+      const now = this.audioContext.currentTime;
       gainNode.gain.setValueAtTime(0, now);
       gainNode.gain.linearRampToValueAtTime(1, now + this.attack);
       gainNode.gain.linearRampToValueAtTime(this.sustain, now + this.attack + this.decay);
       gainNode.gain.setValueAtTime(this.sustain, now + this.attack + this.decay);
       gainNode.gain.linearRampToValueAtTime(0, now + this.attack + this.decay + this.release);
 
-      osc.stop(audioContext.currentTime + this.attack + this.decay + this.release);
+      osc.stop(this.audioContext.currentTime + this.attack + this.decay + this.release);
 
       return osc;
     }
